c4bd1f65210cb0282d41cf3d40ca9b116f8e51f6
Lucky <dprwebdeveloper@gmail.com>
2024-01-10 17:32:46 +0530

    added image

--- src/pages/home/header/Header.js
+++ src/pages/home/header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Header.scss"
+import img1 from "../../../img/hero-img.png"
 const Header = () => {
+const onlineUrl = "https://www.networkgeek.in/dclastfinal3.png"
+
   return (
     <>
       <div className='flexBox'>
@@ -13,7 +16,8 @@ const Header = () => {
         <div className='imgView'>
           <div className='p-5'>
             {/* <img src="/hero-img.png" alt="headerImage" width="450px" height="450px;" /> */}
-            <img src="https://www.networkgeek.in/dclastfinal3.png" alt="headingImagechaged" width="450px" height="380px;" />
+            <img src={img1 || onlineUrl} width="450px" height="450px;" alt="headerImageView" />
+            {/* <img src="https://www.networkgeek.in/dclastfinal3.png" alt="headingImagechaged" width="450px" height="380px;" /> */}
           </div>
         </div>
       </div>
